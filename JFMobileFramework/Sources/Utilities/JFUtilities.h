78e21737f7809a58f4684129c4163f7f1c6af16f
Jacopo Filié <Jackfelle@users.noreply.github.com>
2014-03-30 15:37:24 +0200

    ADD: Added some object equality comparison macros.

--- JFMobileFramework/Sources/Utilities/JFUtilities.h
+++ JFMobileFramework/Sources/Utilities/JFUtilities.h
@@ -17,6 +17,14 @@
 #define	LogMethod	NSLog(@"%@: executing '%@'.", ClassName, MethodName)
 
 
+#pragma mark - Macros (Equality)
+
+#define AreDatesEqual(arg_obj1, arg_obj2)	((!arg_obj1 && !arg_obj2) || [arg_obj1 isEqualToDate:arg_obj2])
+#define AreObjectsEqual(arg_obj1, arg_obj2)	((!arg_obj1 && !arg_obj2) || [arg_obj1 isEqual:arg_obj2])
+#define AreSetsEqual(arg_obj1, arg_obj2)	((!arg_obj1 && !arg_obj2) || [arg_obj1 isEqualToSet:arg_obj2])
+#define AreStringsEqual(arg_obj1, arg_obj2)	((!arg_obj1 && !arg_obj2) || [arg_obj1 isEqualToString:arg_obj2])
+
+
 #pragma mark - Macros (Info)
 
 #define AppDisplayName	appInfoForKey(@"CFBundleDisplayName")
@@ -31,7 +39,7 @@
 
 #pragma mark - Macros (Resources)
 
-#define BundleResourceURL(arg_filename)	[[NSBundle mainBundle] URLForResource:[(arg_filename) stringByDeletingPathExtension] withExtension:[(arg_filename) pathExtension]]
+#define BundleResourceURL(arg_filename)	[[NSBundle mainBundle] URLForResource:[arg_filename stringByDeletingPathExtension] withExtension:[arg_filename pathExtension]]
 
 
 #pragma mark - Macros (Shortcuts)
@@ -42,26 +50,26 @@
 
 #pragma mark - Macros (Strings)
 
-#define BoolToString(arg_val)			((arg_val) ? @"YES" : @"NO")
-#define DoubleToString(arg_val)			[NSString stringWithFormat:@"%f", (double)(arg_val)]
-#define FloatToString(arg_val)			[NSString stringWithFormat:@"%f", (float)(arg_val)]
-#define HexToString(arg_val)			[NSString stringWithFormat:@"%x", (unsigned int)(arg_val)]
-#define	IsEmptyString(arg_str)			[(arg_str) isEqualToString:EmptyString]
-#define	IsNullOrEmptyString(arg_str)	(!(arg_str) || IsEmptyString(arg_str))
+#define BoolToString(arg_val)			(arg_val ? @"YES" : @"NO")
+#define DoubleToString(arg_val)			[NSString stringWithFormat:@"%f", (double)arg_val]
+#define FloatToString(arg_val)			[NSString stringWithFormat:@"%f", (float)arg_val]
+#define HexToString(arg_val)			[NSString stringWithFormat:@"%x", (unsigned int)arg_val]
+#define	IsEmptyString(arg_str)			[arg_str isEqualToString:EmptyString]
+#define	IsNullOrEmptyString(arg_str)	(!arg_str || IsEmptyString(arg_str))
 #define NSIntegerToString(arg_val)		SInt32ToString(arg_val)
 #define NSUIntegerToString(arg_val)		UInt32ToString(arg_val)
-#define	ObjectClassString(arg_obj)		NSStringFromClass([(arg_obj) class])
-#define PointerToString(arg_val)		[NSString stringWithFormat:@"%p", (id)(arg_val)]
-#define SIntToString(arg_val)			[NSString stringWithFormat:@"%d", (int)(arg_val)]
-#define SInt8ToString(arg_val)			[NSString stringWithFormat:@"%hhd", (char)(arg_val)]
-#define SInt16ToString(arg_val)			[NSString stringWithFormat:@"%hd", (short)(arg_val)]
-#define SInt32ToString(arg_val)			[NSString stringWithFormat:@"%ld", (long)(arg_val)]
-#define SInt64ToString(arg_val)			[NSString stringWithFormat:@"%lld", (long long)(arg_val)]
-#define UIntToString(arg_val)			[NSString stringWithFormat:@"%u", (unsigned int)(arg_val)]
-#define UInt8ToString(arg_val)			[NSString stringWithFormat:@"%hhu", (unsigned char)(arg_val)]
-#define UInt16ToString(arg_val)			[NSString stringWithFormat:@"%hu", (unsigned short)(arg_val)]
-#define UInt32ToString(arg_val)			[NSString stringWithFormat:@"%lu", (unsigned long)(arg_val)]
-#define UInt64ToString(arg_val)			[NSString stringWithFormat:@"%llu", (unsigned long long)(arg_val)]
+#define	ObjectClassString(arg_obj)		NSStringFromClass([arg_obj class])
+#define PointerToString(arg_val)		[NSString stringWithFormat:@"%p", (id)arg_val]
+#define SIntToString(arg_val)			[NSString stringWithFormat:@"%d", (int)arg_val]
+#define SInt8ToString(arg_val)			[NSString stringWithFormat:@"%hhd", (char)arg_val]
+#define SInt16ToString(arg_val)			[NSString stringWithFormat:@"%hd", (short)arg_val]
+#define SInt32ToString(arg_val)			[NSString stringWithFormat:@"%ld", (long)arg_val]
+#define SInt64ToString(arg_val)			[NSString stringWithFormat:@"%lld", (long long)arg_val]
+#define UIntToString(arg_val)			[NSString stringWithFormat:@"%u", (unsigned int)arg_val]
+#define UInt8ToString(arg_val)			[NSString stringWithFormat:@"%hhu", (unsigned char)arg_val]
+#define UInt16ToString(arg_val)			[NSString stringWithFormat:@"%hu", (unsigned short)arg_val]
+#define UInt32ToString(arg_val)			[NSString stringWithFormat:@"%lu", (unsigned long)arg_val]
+#define UInt64ToString(arg_val)			[NSString stringWithFormat:@"%llu", (unsigned long long)arg_val]
 
 
 #pragma mark - Macros (User interface)
