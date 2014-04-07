74b9c0a725573d2e1af8e8bc75ccdd77e44f00c7
Jacopo Filié <Jackfelle@users.noreply.github.com>
2014-04-08 01:08:51 +0200

    MOD: Worked on the "JFSimpleFormViewController" class.

--- JFMobileFramework/Sources/User Interface/JFSimpleFormViewController.h
+++ JFMobileFramework/Sources/User Interface/JFSimpleFormViewController.h
@@ -12,6 +12,39 @@
 
 
 
+// Form cell styles
+typedef NS_ENUM(NSUInteger, JFSimpleFormViewCellStyle) {
+	JFSimpleFormViewCellStyleField,
+	JFSimpleFormViewCellStyleLabel,
+	JFSimpleFormViewCellStyleImage,
+	JFSimpleFormViewCellStyleText,
+};
+
+// Default insets
+extern UIEdgeInsets const fieldInsets;
+extern UIEdgeInsets const labelInsets;
+extern UIEdgeInsets const imageInsets;
+extern UIEdgeInsets const textInsets;
+
+
+
 @interface JFSimpleFormViewController : UITableViewController
+
+@end
+
+
+
+@interface JFSimpleFormViewCellInfo : NSObject
+
+// Attributes
+@property (assign, nonatomic)	UIEdgeInsets				insets;
+@property (assign, nonatomic)	JFSimpleFormViewCellStyle	style;
+
+// Data
+@property (copy, nonatomic)		NSString*	text;
+@property (copy, nonatomic)		NSString*	title;
+
+// Memory management
+- (instancetype)	initWithStyle:(JFSimpleFormViewCellStyle)style;
 
 @end
--- JFMobileFramework/Sources/User Interface/JFSimpleFormViewController.m
+++ JFMobileFramework/Sources/User Interface/JFSimpleFormViewController.m
@@ -14,58 +14,11 @@
 
 
 
-static UIEdgeInsets labelInsets = {0.0f, 5.0f, 0.0f, 5.0f};
-
-
-
-typedef NS_ENUM(NSUInteger, JFSimpleFormViewCellStyle) {
-	JFSimpleFormViewCellStyleField,
-	JFSimpleFormViewCellStyleLabel,
-	JFSimpleFormViewCellStyleImage,
-	JFSimpleFormViewCellStyleText,
-};
-
-
-
-@interface JFObject : NSObject
-
-// Attributes
-@property (assign, nonatomic)	JFSimpleFormViewCellStyle	style;
-
-// Data
-@property (copy, nonatomic)	NSString*	text;
-@property (copy, nonatomic)	NSString*	title;
-
-@end
-
-
-
-@implementation JFObject
-
-#pragma mark - Properties
-
-// Attributes
-@synthesize style	= _style;
-
-// Data
-@synthesize text	= _text;
-@synthesize title	= _title;
-
-
-#pragma mark - Memory management
-
-- (instancetype)init
-{
-	self = [super init];
-	if(self)
-	{
-		// Attributes
-		_style = JFSimpleFormViewCellStyleLabel;
-    }
-	return self;
-}
-
-@end
+// Default insets
+UIEdgeInsets const fieldInsets	= {0.0f, 0.0f, 0.0f, 0.0f};
+UIEdgeInsets const labelInsets	= {0.0f, 10.0f, 0.0f, 10.0f};
+UIEdgeInsets const imageInsets	= {0.0f, 0.0f, 0.0f, 0.0f};
+UIEdgeInsets const textInsets	= {0.0f, 0.0f, 0.0f, 0.0f};
 
 
 
@@ -74,10 +27,15 @@ typedef NS_ENUM(NSUInteger, JFSimpleFormViewCellStyle) {
 // Data
 @property (strong, nonatomic, readonly)	NSMutableArray*	tableData;
 
+// User interface management
+- (UITextField*)	createSimpleFormViewCellField;
+- (UIImageView*)	createSimpleFormViewCellImage;
+- (UILabel*)		createSimpleFormViewCellLabel;
+- (UITextView*)		createSimpleFormViewCellText;
+
 // Table view management
-- (UILabel*)			createSimpleFormViewCellLabelWithFrame:(CGRect)frame;
 - (UITableViewCell*)	createSimpleFormViewCellWithStyle:(JFSimpleFormViewCellStyle)style reuseIdentifier:(NSString*)reuseIdentifier;
-- (void)				fillSimpleFormViewCell:(UITableViewCell*)cell withInfoForObject:(JFObject*)object;
+- (void)				fillSimpleFormViewCell:(UITableViewCell*)cell withInfo:(JFSimpleFormViewCellInfo*)info;
 
 @end
 
@@ -131,14 +89,47 @@ typedef NS_ENUM(NSUInteger, JFSimpleFormViewCellStyle) {
 {
 	[super viewDidLoad];
 	
-	JFObject* o1 = [JFObject new];
+	JFSimpleFormViewCellInfo* o1 = [JFSimpleFormViewCellInfo new];
 	o1.title = @"Ingredienti";
 	o1.text = @"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sed quam dui. Ut imperdiet lacus sed rutrum mattis. Fusce ut facilisis mi. Nullam a sagittis ligula. Nunc dictum diam nec luctus dapibus. Vestibulum ac ultricies nibh. Etiam id risus convallis, sollicitudin sem a, consectetur leo. Etiam purus mi, fermentum ut euismod sit amet, cursus nec dui. Donec consectetur sem vel justo dignissim dapibus.";
-	JFObject* o2 = [JFObject new];
+	JFSimpleFormViewCellInfo* o2 = [[JFSimpleFormViewCellInfo alloc] initWithStyle:JFSimpleFormViewCellStyleLabel];
 	o2.title = @"Istruzioni";
 	o2.text = @"Integer vel ornare ante. In id leo sit amet tellus feugiat varius vitae vel nulla. Nullam laoreet tortor quam, sed rutrum tellus aliquet vitae. Mauris et orci risus. Vestibulum in neque lacus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse vitae consectetur quam. Pellentesque vel imperdiet elit, nec posuere enim. Praesent hendrerit metus arcu, at semper diam vulputate in. Fusce in urna turpis. Nullam luctus mollis nisi non consectetur. Mauris vel lacus ut arcu rutrum feugiat nec nec elit.";
-	[self.tableData setArray:@[o1, o2]];
+	JFSimpleFormViewCellInfo* o3 = [[JFSimpleFormViewCellInfo alloc] initWithStyle:JFSimpleFormViewCellStyleImage];
+	o3.title = @"Immagine";
+	[self.tableData setArray:@[o1, o2, o3]];
 	[self.tableView reloadData];
+}
+
+
+#pragma mark - User interface management
+
+- (UITextField*)createSimpleFormViewCellField
+{
+	UITextField* retVal = [[UITextField alloc] init];
+	return retVal;
+}
+
+- (UIImageView*)createSimpleFormViewCellImage
+{
+	UIImageView* retVal = [[UIImageView alloc] init];
+	return retVal;
+}
+
+- (UILabel*)createSimpleFormViewCellLabel
+{
+	UILabel* retVal = [[UILabel alloc] init];
+	retVal.backgroundColor = [UIColor cyanColor];
+	retVal.font = [UIFont systemFontOfSize:16.0f];
+	retVal.lineBreakMode = NSLineBreakByWordWrapping;
+	retVal.numberOfLines = 0;
+	return retVal;
+}
+
+- (UITextView*)createSimpleFormViewCellText
+{
+	UITextView* retVal = [[UITextView alloc] init];
+	return retVal;
 }
 
 
@@ -156,23 +147,23 @@ typedef NS_ENUM(NSUInteger, JFSimpleFormViewCellStyle) {
 
 - (UITableViewCell*)tableView:(UITableView*)tableView cellForRowAtIndexPath:(NSIndexPath*)indexPath
 {
-	JFObject* object = [self.tableData objectAtIndex:indexPath.section];
+	JFSimpleFormViewCellInfo* info = [self.tableData objectAtIndex:indexPath.section];
 	
-	NSString* const CellIdentifier = NSUIntegerToString(object.style);
+	NSString* const CellIdentifier = NSUIntegerToString(info.style);
 	
     UITableViewCell* retVal = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
 	if(!retVal)
-		retVal = [self createSimpleFormViewCellWithStyle:object.style reuseIdentifier:CellIdentifier];
+		retVal = [self createSimpleFormViewCellWithStyle:info.style reuseIdentifier:CellIdentifier];
 	
-	[self fillSimpleFormViewCell:retVal withInfoForObject:object];
+	[self fillSimpleFormViewCell:retVal withInfo:info];
     
     return retVal;
 }
 
 - (NSString*)tableView:(UITableView*)tableView titleForHeaderInSection:(NSInteger)section
 {
-	JFObject* object = [self.tableData objectAtIndex:section];
-	return object.title;
+	JFSimpleFormViewCellInfo* info = [self.tableData objectAtIndex:section];
+	return info.title;
 }
 
 
@@ -185,25 +176,26 @@ typedef NS_ENUM(NSUInteger, JFSimpleFormViewCellStyle) {
 
 - (CGFloat)tableView:(UITableView*)tableView heightForRowAtIndexPath:(NSIndexPath*)indexPath
 {
-	JFObject* object = [self.tableData objectAtIndex:indexPath.section];
+	JFSimpleFormViewCellInfo* info = [self.tableData objectAtIndex:indexPath.section];
 	
 	CGFloat retVal = UITableViewAutomaticDimension;
-	switch(object.style)
+	switch(info.style)
 	{
 		case JFSimpleFormViewCellStyleLabel:
 		{
 			CGRect frame = self.tableView.bounds;
-			frame.size.height -= labelInsets.top + labelInsets.bottom;
-			frame.size.width -= labelInsets.left + labelInsets.right;
+			frame.size.height -= info.insets.top + info.insets.bottom;
+			frame.size.width -= info.insets.left + info.insets.right;
 			
-			UILabel* label = [self createSimpleFormViewCellLabelWithFrame:frame];
-			label.text = object.text;
+			UILabel* label = [self createSimpleFormViewCellLabel];
+			label.frame = frame;
+			label.text = info.text;
 			
-			CGSize limits = CGSizeMake(self.view.bounds.size.width - (labelInsets.left + labelInsets.right), CGFLOAT_MAX);
+			CGSize limits = CGSizeMake(frame.size.width, CGFLOAT_MAX);
 			CGSize size = [label sizeThatFits:limits];
-			size.height += (iOS6 ? 0.0f : 1.0f);
+			size.height += (iOS6 ? 0.0f : 1.0f); // Seems to fix an error in the "sizeThatFits:" method of the iOS7+ SDK.
 			
-			retVal = size.height + labelInsets.top + labelInsets.bottom;
+			retVal = size.height + info.insets.top + info.insets.bottom;
 			break;
 		}
 		default:
@@ -216,50 +208,26 @@ typedef NS_ENUM(NSUInteger, JFSimpleFormViewCellStyle) {
 
 #pragma mark - Table view management
 
-- (UILabel*)createSimpleFormViewCellLabelWithFrame:(CGRect)frame
-{
-	UILabel* retVal = [[UILabel alloc] initWithFrame:frame];
-	retVal.backgroundColor = [UIColor cyanColor];
-	retVal.font = [UIFont systemFontOfSize:16.0f];
-	retVal.lineBreakMode = NSLineBreakByWordWrapping;
-	retVal.numberOfLines = 0;
-	return retVal;
-}
-
 - (UITableViewCell*)createSimpleFormViewCellWithStyle:(JFSimpleFormViewCellStyle)style reuseIdentifier:(NSString*)reuseIdentifier
 {
 	UITableViewCell* retVal = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:reuseIdentifier];
 	if(!retVal)
 		return nil;
 	
-	CGRect frame = retVal.contentView.bounds;
 	UIView* view = nil;
 	switch(style)
 	{
 		case JFSimpleFormViewCellStyleField:
 		{
-			UITextField* textField = [[UITextField alloc] initWithFrame:frame];
+			UITextField* textField = [[UITextField alloc] init];
 			view = textField;
 			break;
 		}
-		case JFSimpleFormViewCellStyleImage:
-		{
-			UIImageView* imageView = [[UIImageView alloc] initWithFrame:frame];
-			view = imageView;
-			break;
-		}
-		case JFSimpleFormViewCellStyleLabel:
-		{
-			frame.origin.x += labelInsets.left;
-			frame.origin.y += labelInsets.top;
-			frame.size.height -= labelInsets.top + labelInsets.bottom;
-			frame.size.width -= labelInsets.left + labelInsets.right;
-			view = [self createSimpleFormViewCellLabelWithFrame:frame];
-			break;
-		}
+		case JFSimpleFormViewCellStyleImage:	view = [self createSimpleFormViewCellImage];	break;
+		case JFSimpleFormViewCellStyleLabel:	view = [self createSimpleFormViewCellLabel];	break;
 		case JFSimpleFormViewCellStyleText:
 		{
-			UITextView* textView = [[UITextView alloc] initWithFrame:frame];
+			UITextView* textView = [[UITextView alloc] init];
 			view = textView;
 			break;
 		}
@@ -270,6 +238,7 @@ typedef NS_ENUM(NSUInteger, JFSimpleFormViewCellStyle) {
 	if(view)
 	{
 		view.autoresizingMask = (UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth);
+		view.frame = retVal.contentView.bounds;
 		view.tag = 1;
 		
 		[retVal.contentView addSubview:view];
@@ -278,10 +247,17 @@ typedef NS_ENUM(NSUInteger, JFSimpleFormViewCellStyle) {
 	return retVal;
 }
 
-- (void)fillSimpleFormViewCell:(UITableViewCell*)cell withInfoForObject:(JFObject*)object
+- (void)fillSimpleFormViewCell:(UITableViewCell*)cell withInfo:(JFSimpleFormViewCellInfo*)info
 {
 	UIView* view = [cell viewWithTag:1];
-	switch(object.style)
+	CGRect frame = view.superview.bounds;
+	frame.origin.x += info.insets.left;
+	frame.origin.y += info.insets.top;
+	frame.size.width -= info.insets.left + info.insets.right;
+	frame.size.height -= info.insets.top + info.insets.bottom;
+	view.frame = frame;
+	
+	switch(info.style)
 	{
 		case JFSimpleFormViewCellStyleField:
 		{
@@ -290,13 +266,18 @@ typedef NS_ENUM(NSUInteger, JFSimpleFormViewCellStyle) {
 		}
 		case JFSimpleFormViewCellStyleImage:
 		{
-			//UIImageView* imageView = (UIImageView*)view;
+			NSString* format = [NSString stringWithFormat:@"%gx%g", frame.size.width, frame.size.height];
+			NSString* urlString = [NSString stringWithFormat:@"http://placehold.it/%@/", format];
+			NSURL* url = [NSURL URLWithString:urlString];
+			NSData* data = [NSData dataWithContentsOfURL:url];
+			UIImageView* imageView = (UIImageView*)view;
+			imageView.image = [UIImage imageWithData:data];
 			break;
 		}
 		case JFSimpleFormViewCellStyleLabel:
 		{
 			UILabel* label = (UILabel*)view;
-			label.text = object.text;
+			label.text = info.text;
 			break;
 		}
 		case JFSimpleFormViewCellStyleText:
@@ -307,6 +288,54 @@ typedef NS_ENUM(NSUInteger, JFSimpleFormViewCellStyle) {
 		default:
 			break;
 	}
+}
+
+@end
+
+
+
+@implementation JFSimpleFormViewCellInfo
+
+#pragma mark - Properties
+
+// Attributes
+@synthesize insets	= _insets;
+@synthesize style	= _style;
+
+// Data
+@synthesize text	= _text;
+@synthesize title	= _title;
+
+
+#pragma mark - Memory management
+
+- (instancetype)init
+{
+	return [self initWithStyle:JFSimpleFormViewCellStyleLabel];
+}
+
+- (instancetype)initWithStyle:(JFSimpleFormViewCellStyle)style
+{
+	UIEdgeInsets insets = UIEdgeInsetsZero;
+	switch(style)
+	{
+		case JFSimpleFormViewCellStyleField:	insets = fieldInsets;	break;
+		case JFSimpleFormViewCellStyleImage:	insets = imageInsets;	break;
+		case JFSimpleFormViewCellStyleLabel:	insets = labelInsets;	break;
+		case JFSimpleFormViewCellStyleText:		insets = textInsets;	break;
+			
+		default:
+			break;
+	}
+	
+	self = [super init];
+	if(self)
+	{
+		// Attributes
+		_insets = insets;
+		_style = style;
+    }
+	return self;
 }
 
 @end
