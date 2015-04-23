e4e5d0573a2cb24abe36b5f220aca056dc776160
Jacopo Filié <Jackfelle@users.noreply.github.com>
2015-04-24 00:14:39 +0200

    ADD: added the "JFSplashViewController" class.

---
+++ JFFramework/Sources/User Interface/JFSplashViewController.h
@@ -0,0 +1,66 @@
+//
+//  JFSplashViewController.h
+//  Copyright (C) 2015  Jacopo Filié
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+
+
+#import "JFViewController.h"
+
+
+
+@class JFSplashViewController;
+
+
+
+@protocol JFSplashViewControllerDelegate <NSObject>
+
+@optional
+
+- (void)	splashViewControllerTapGestureRecognized:(JFSplashViewController*)viewController;
+- (void)	splashViewControllerTimedOut:(JFSplashViewController*)viewController;
+
+@end
+
+
+
+#pragma mark
+
+
+
+@interface JFSplashViewController : JFViewController
+
+#pragma mark Properties
+
+// Flags
+@property (assign, nonatomic, getter = isUserInteractionEnabled)	BOOL	userInteractionEnabled;
+
+// Relationships
+@property (weak, nonatomic)	id<JFSplashViewControllerDelegate>	delegate;
+
+// Timings
+@property (assign, nonatomic)	NSTimeInterval	timeout;
+
+// User interface
+@property (strong, nonatomic, readonly)	UIImageView*	imageView;
+
+
+#pragma mark Methods
+
+// User interface management (Actions)
+- (IBAction)	tapGestureRecognized:(UITapGestureRecognizer*)sender;
+
+@end
