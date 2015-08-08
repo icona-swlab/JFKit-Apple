fd95b072fb17da0e84888f5498bfc054b4761be7
Jacopo Filié <Jackfelle@users.noreply.github.com>
2015-08-09 01:47:02 +0200

    ADD: added the new framework project.

---
+++ JFFramework/Supporting Files/JFFramework.h
@@ -0,0 +1,55 @@
+//
+//  JFFramework.h
+//  Copyright (C) 2015  Jacopo Filié
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+
+
+#pragma mark - Framework version constants
+
+#if TARGET_OS_IPHONE
+
+//! Project version number for JFFramework-iOS.
+FOUNDATION_EXPORT double JFFramework_iOSVersionNumber;
+
+//! Project version string for JFFramework-iOS.
+FOUNDATION_EXPORT const unsigned char JFFramework_iOSVersionString[];
+
+#else
+
+//! Project version number for JFFramework-OSX.
+FOUNDATION_EXPORT double JFFramework_OSXVersionNumber;
+
+//! Project version string for JFFramework-OSX.
+FOUNDATION_EXPORT const unsigned char JFFramework_OSXVersionString[];
+
+#endif
+
+
+
+#pragma mark - Common headers
+
+#if TARGET_OS_IPHONE
+#pragma mark - iOS specific headers
+
+// In this header, you should import all the public headers of your framework using statements like #import <JFFramework_iOS/PublicHeader.h>
+
+#else
+#pragma mark - OS X specific headers
+
+// In this header, you should import all the public headers of your framework using statements like #import <JFFramework_OSX/PublicHeader.h>
+
+#endif
